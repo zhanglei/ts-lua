3a239c57660926e556c8ab9c62a9b4eac225145c
Quehan <quehan@taobao.com>
2014-02-10 15:39:16 +0800

    add ts.fetch

---
+++ src/ts_lua_fetch.c
@@ -0,0 +1,263 @@
+/*
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+
+#include <arpa/inet.h>
+#include <ts_fetcher/ts_fetcher.h>
+#include "ts_lua_util.h"
+
+typedef struct {
+    TSIOBuffer          buffer;
+    TSIOBufferReader    reader;
+    ts_lua_http_intercept_ctx   *ictx;
+} ts_lua_fetch_ctx;
+
+static int ts_lua_fetch(lua_State *L);
+
+static int ts_lua_fetch_handler(TSCont contp, TSEvent event, void *edata);
+static int ts_lua_fetch_cleanup(struct ict_item *item);
+
+
+void
+ts_lua_inject_fetch_api(lua_State *L)
+{
+    lua_pushcfunction(L, ts_lua_fetch);
+    lua_setfield(L, -2, "fetch");
+}
+
+static int
+ts_lua_fetch(lua_State *L)
+{
+    TSCont              contp;
+    http_fetcher        *fch;
+    const char          *url, *value, *key;
+    size_t              len, val_len, key_len;
+    const char          *method, *body;
+    size_t              method_len, body_len;
+    int                 base;
+    struct sockaddr      client_addr;
+    ts_lua_fetch_ctx    *fctx;
+    ts_lua_http_intercept_item  *item;
+    ts_lua_http_intercept_ctx   *ictx;
+
+    ictx = ts_lua_get_http_intercept_ctx(L);
+    client_addr = *TSHttpTxnClientAddrGet(ictx->hctx->txnp);
+
+    url = luaL_checklstring(L, 1, &len);
+
+    if (!lua_istable(L, 2)) {
+        return luaL_error(L, "The 2nd param of ts.fetch is not a table.\n");
+    }
+
+    contp = TSContCreate(ts_lua_fetch_handler, TSContMutexGet(ictx->contp));
+
+    base = lua_gettop(L);
+
+    /* option */
+    fch = ts_http_fetcher_create(contp, &client_addr, 0);
+
+    /* method */
+    lua_pushlstring(L, "method", sizeof("method")-1);
+    lua_gettable(L, 2);
+
+    if (lua_isstring(L, -1)) {
+        method = lua_tolstring(L, -1, &method_len);
+        ts_http_fetcher_init(fch, method, method_len, url, len);
+
+    } else {
+        ts_http_fetcher_init_common(fch, TS_FETCH_METHOD_GET, url, len);
+    }
+
+    lua_pop(L, 1);
+
+    /* header */
+    lua_pushlstring(L, "header", sizeof("header")-1);
+    lua_gettable(L, 2);
+
+    if (lua_istable(L, -1)) {
+        lua_pushnil(L);
+
+        while (lua_next(L, -2)) {
+            value = luaL_checklstring (L, -1, &val_len);
+            key = luaL_checklstring (L, -2, &key_len);
+
+            ts_http_fetcher_add_header(fch, key, key_len, value, val_len);
+            lua_pop(L, 1);
+        }
+    }
+
+    lua_pop(L, 1);
+
+    /* body */
+    lua_pushlstring(L, "body", sizeof("body")-1);
+    lua_gettable(L, 2);
+
+    if (lua_isstring(L, -1)) {
+        body = lua_tolstring(L, -1, &body_len);
+        ts_http_fetcher_append_data(fch, body, body_len);
+    }
+
+    lua_pop(L, 1);
+
+    fctx = (ts_lua_fetch_ctx*)TSmalloc(sizeof(ts_lua_fetch_ctx));
+    fctx->buffer = TSIOBufferCreate();
+    fctx->reader = TSIOBufferReaderAlloc(fctx->buffer);
+    fctx->ictx = ictx;
+
+    item = (ts_lua_http_intercept_item*)TSmalloc(sizeof(ts_lua_http_intercept_item));
+    TS_LUA_ADD_INTERCEPT_ITEM(ictx, item, contp, ts_lua_fetch_cleanup, fch);
+
+    ts_http_fetcher_set_ctx(fch, fctx);
+    TSContDataSet(contp, item);
+
+    ts_http_fetcher_launch(fch);
+
+    lua_settop(L, base);
+
+    return lua_yield(L, 0);
+}
+
+static int
+ts_lua_fetch_handler(TSCont contp, TSEvent event, void *edata)
+{
+    int64_t             avail, all;
+    int64_t             blk_len, already;
+    char                *data;
+    const char          *start;
+    const char          *name, *value;
+    int                 name_len, value_len;
+    lua_State           *L;
+    ts_lua_fetch_ctx    *fctx;
+    http_fetcher        *fch;
+    TSIOBufferBlock     blk;
+    TSMLoc              field_loc, next_field_loc;
+    ts_lua_http_intercept_item  *item;
+    ts_lua_http_intercept_ctx   *ictx;
+
+    fch = (http_fetcher*)edata;
+    fctx = (ts_lua_fetch_ctx*)ts_http_fetcher_get_ctx(fch);
+    ictx = fctx->ictx;
+    L = ictx->lua;
+
+    switch (event) {
+
+        case TS_EVENT_FETCH_HEADER_DONE:
+            break;
+
+        case TS_EVENT_FETCH_BODY_READY:
+        case TS_EVENT_FETCH_BODY_COMPLETE:
+            avail = TSIOBufferReaderAvail(fch->body_reader);
+            TSIOBufferCopy(fctx->buffer, fch->body_reader, avail, 0);
+            ts_http_fetcher_consume_resp_body(fch, avail);
+
+            if (event == TS_EVENT_FETCH_BODY_COMPLETE) {
+                lua_newtable(L);
+
+                /* status */
+                lua_pushlstring(L, "status", sizeof("status")-1);
+                lua_pushnumber(L, fch->status_code);
+                lua_settable(L, -3);
+
+                /* body */
+                all = TSIOBufferReaderAvail(fctx->reader);
+                data = TSmalloc(all);
+
+                already = 0;
+                blk = TSIOBufferReaderStart(fctx->reader);
+
+                while (blk) {
+                    start = TSIOBufferBlockReadStart(blk, fctx->reader, &blk_len);
+                    memcpy(data + already, start, blk_len);
+                    already += blk_len;
+                    blk = TSIOBufferBlockNext(blk);
+                }
+
+                lua_pushlstring(L, "body", sizeof("body")-1);
+                lua_pushlstring(L, data, all);
+                lua_settable(L, -3);
+
+                /* header */
+                lua_pushlstring(L, "header", sizeof("header")-1);
+                lua_newtable(L);
+
+                field_loc = TSMimeHdrFieldGet(fch->hdr_bufp, fch->hdr_loc, 0);
+                while (field_loc) {
+                    name = TSMimeHdrFieldNameGet(fch->hdr_bufp, fch->hdr_loc, field_loc, &name_len);
+                    if (name) {
+                        value = TSMimeHdrFieldValueStringGet(fch->hdr_bufp, fch->hdr_loc, field_loc, -1, &value_len);
+                        lua_pushlstring(L, name, name_len);
+                        lua_pushlstring(L, value, value_len);
+                        lua_settable(L, -3);
+                    }
+
+                    next_field_loc = TSMimeHdrFieldNext(fch->hdr_bufp, fch->hdr_loc, field_loc);
+                    TSHandleMLocRelease(fch->hdr_bufp, fch->hdr_loc, field_loc);
+                    field_loc = next_field_loc;
+                }
+
+                lua_settable(L, -3);
+
+                /* final */
+                TSfree(data);
+
+                item = (ts_lua_http_intercept_item*)TSContDataGet(contp);
+                ts_lua_fetch_cleanup(item);
+                TSContCall(ictx->contp, event, (void*)1);
+            }
+
+            break;
+
+        case TS_EVENT_FETCH_ERROR:
+        default:
+
+            lua_newtable(L);
+            /* err */
+            lua_pushlstring(L, "err", sizeof("err")-1);
+            lua_pushnumber(L, 1);
+            lua_settable(L, -3);
+
+            item = (ts_lua_http_intercept_item*)TSContDataGet(contp);
+            ts_lua_fetch_cleanup(item);
+            TSContCall(ictx->contp, event, (void*)1);
+            return -1;
+    }
+
+    return 0;
+}
+
+static int
+ts_lua_fetch_cleanup(struct ict_item *item)
+{
+    http_fetcher        *fch;
+    ts_lua_fetch_ctx    *fctx;
+
+    fch = (http_fetcher*)item->data;
+    fctx = (ts_lua_fetch_ctx*)ts_http_fetcher_get_ctx(fch);
+
+    TSIOBufferReaderFree(fctx->reader);
+    TSIOBufferDestroy(fctx->buffer);
+
+    TSfree(fctx);
+
+    ts_http_fetcher_destroy(fch);
+    TSContDestroy(item->contp);
+
+    item->deleted = 1;
+    return 0;
+}
+
